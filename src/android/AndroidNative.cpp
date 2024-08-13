d299bcdbd825af75825f23d469724bb0c8fc8f7e
Yancey <1709185482@qq.com>
2024-08-13 10:53:26 +0800

    update CHelper-Android

--- src/android/AndroidNative.cpp
+++ src/android/AndroidNative.cpp
@@ -82,26 +82,31 @@ Java_yancey_chelper_core_CHelperCore_getDescription0(
     return env->NewStringUTF(core->getDescription().c_str());
 }
 
-extern "C" [[maybe_unused]] JNIEXPORT jstring JNICALL
+extern "C" [[maybe_unused]] JNIEXPORT jobjectArray JNICALL
 Java_yancey_chelper_core_CHelperCore_getErrorReasons0(
         JNIEnv *env, [[maybe_unused]] jobject thiz, jlong pointer) {
     auto *core = reinterpret_cast<CHelper::Core *>(pointer);
+    jclass errorReasonClass = env->FindClass("yancey/chelper/core/ErrorReason");
     if (HEDLEY_UNLIKELY(core == nullptr)) {
-        return nullptr;
+        return env->NewObjectArray(0, errorReasonClass, nullptr);
     }
     auto errorReasons = core->getErrorReasons();
-    if (HEDLEY_UNLIKELY(errorReasons.empty())) {
-        return nullptr;
-    } else if (HEDLEY_UNLIKELY(errorReasons.size() == 1)) {
-        return env->NewStringUTF(errorReasons[0]->errorReason.c_str());
-    } else {
-        std::string result = "可能的错误原因：";
-        int i = 0;
-        for (const auto &item: errorReasons) {
-            result.append("\n").append(std::to_string(++i)).append(". ").append(item->errorReason);
-        }
-        return env->NewStringUTF(result.c_str());
+    jobjectArray result = env->NewObjectArray(static_cast<jsize>(errorReasons.size()), errorReasonClass, nullptr);
+    for (int i = 0; i < errorReasons.size(); ++i) {
+        const CHelper::ErrorReason &item = *errorReasons[i];
+        jobject javaErrorReason = env->AllocObject(errorReasonClass);
+        env->SetObjectField(javaErrorReason,
+                            env->GetFieldID(errorReasonClass, "errorReason", "Ljava/lang/String;"),
+                            env->NewStringUTF(item.errorReason.c_str()));
+        env->SetLongField(javaErrorReason,
+                            env->GetFieldID(errorReasonClass, "start", "J"),
+                            static_cast<jlong>(item.start));
+        env->SetLongField(javaErrorReason,
+                         env->GetFieldID(errorReasonClass, "end", "J"),
+                         static_cast<jlong>(item.end));
+        env->SetObjectArrayElement(result, i, javaErrorReason);
     }
+    return result;
 }
 
 extern "C" [[maybe_unused]] JNIEXPORT jlong JNICALL
@@ -126,47 +131,46 @@ Java_yancey_chelper_core_CHelperCore_getSuggestion0(
         return nullptr;
     }
     CHelper::Suggestion suggestion = suggestions->at(which);
-    jobject javaDataComplete = env->AllocObject(env->FindClass("yancey/chelper/core/Suggestion"));
-    env->SetObjectField(javaDataComplete,
-                        env->GetFieldID(env->GetObjectClass(javaDataComplete), "name", "Ljava/lang/String;"),
+    jclass suggestionClass = env->FindClass("yancey/chelper/core/Suggestion");
+    jobject javaSuggestion = env->AllocObject(suggestionClass);
+    env->SetObjectField(javaSuggestion,
+                        env->GetFieldID(suggestionClass, "name", "Ljava/lang/String;"),
                         env->NewStringUTF(suggestion.content->name.c_str()));
-    env->SetObjectField(javaDataComplete,
-                        env->GetFieldID(env->GetObjectClass(javaDataComplete), "description", "Ljava/lang/String;"),
+    env->SetObjectField(javaSuggestion,
+                        env->GetFieldID(suggestionClass, "description", "Ljava/lang/String;"),
                         suggestion.content->description.has_value()
                                 ? env->NewStringUTF(suggestion.content->description.value().c_str())
                                 : nullptr);
-    return javaDataComplete;
+    return javaSuggestion;
 }
 
 extern "C" [[maybe_unused]] JNIEXPORT jobject JNICALL
 Java_yancey_chelper_core_CHelperCore_getSuggestions0(
         JNIEnv *env, [[maybe_unused]] jobject thiz, jlong pointer) {
     auto *core = reinterpret_cast<CHelper::Core *>(pointer);
+    jclass suggestionClass = env->FindClass("yancey/chelper/core/Suggestion");
     if (HEDLEY_UNLIKELY(core == nullptr)) {
-        return nullptr;
+        return env->NewObjectArray(0, suggestionClass, nullptr);
     }
-    auto suggestions = core->getSuggestions();
-    if (HEDLEY_UNLIKELY(suggestions->empty())) {
-        return nullptr;
+    const std::vector<CHelper::Suggestion> &suggestions = *core->getSuggestions();
+    if (HEDLEY_UNLIKELY(suggestions.empty())) {
+        return env->NewObjectArray(0, suggestionClass, nullptr);
     }
-    jclass arrayListClass = env->FindClass("java/util/ArrayList");
-    jmethodID arrayListConstructor = env->GetMethodID(arrayListClass, "<init>", "()V");
-    jmethodID arrayListAdd = env->GetMethodID(arrayListClass, "add", "(Ljava/lang/Object;)Z");
-    jclass dataCompleteClass = env->FindClass("yancey/chelper/core/Suggestion");
-    jobject javaList = env->NewObject(arrayListClass, arrayListConstructor);
-    for (const auto &item: *suggestions) {
-        jobject javaDataComplete = env->AllocObject(dataCompleteClass);
-        env->SetObjectField(javaDataComplete,
-                            env->GetFieldID(env->GetObjectClass(javaDataComplete), "name", "Ljava/lang/String;"),
+    jobjectArray result = env->NewObjectArray(suggestions.size(), suggestionClass, nullptr);
+    for (int i = 0; i < suggestions.size(); ++i) {
+        const CHelper::Suggestion &item = suggestions[i];
+        jobject javaSuggestion = env->AllocObject(suggestionClass);
+        env->SetObjectField(javaSuggestion,
+                            env->GetFieldID(suggestionClass, "name", "Ljava/lang/String;"),
                             env->NewStringUTF(item.content->name.c_str()));
-        env->SetObjectField(javaDataComplete,
-                            env->GetFieldID(env->GetObjectClass(javaDataComplete), "description", "Ljava/lang/String;"),
+        env->SetObjectField(javaSuggestion,
+                            env->GetFieldID(suggestionClass, "description", "Ljava/lang/String;"),
                             item.content->description.has_value()
                                     ? env->NewStringUTF(item.content->description.value().c_str())
                                     : nullptr);
-        env->CallBooleanMethod(javaList, arrayListAdd, javaDataComplete);
+        env->SetObjectArrayElement(result, i, javaSuggestion);
     }
-    return javaList;
+    return result;
 }
 
 extern "C" [[maybe_unused]] JNIEXPORT jstring JNICALL
