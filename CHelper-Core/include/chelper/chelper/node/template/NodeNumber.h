fa330e92ccaed1b36dc1f2541f47dbb6d3c5c330
Yancey <1709185482@qq.com>
2024-08-21 08:15:56 +0800

    fix NodeNumber.h

--- CHelper-Core/include/chelper/chelper/node/template/NodeNumber.h
+++ CHelper-Core/include/chelper/chelper/node/template/NodeNumber.h
@@ -16,15 +16,11 @@ namespace CHelper::Node {
 
         NodeNumber() = default;
 
-        [[nodiscard]] NodeType *getNodeType() const override {
-            return NodeType::JSON_INTEGER.get();
-        }
-
         ASTNode getASTNode(TokenReader &tokenReader, const CPack *cpack) const override {
-            if constexpr (std::is_same<T, int32_t>()) {
-                return tokenReader.readIntegerASTNode(this);
-            } else {
+            if constexpr (std::is_same<T, float>() || std::is_same<T, double>()) {
                 return tokenReader.readFloatASTNode(this);
+            } else {
+                return tokenReader.readIntegerASTNode(this);
             }
         }
 
@@ -34,22 +30,36 @@ namespace CHelper::Node {
                 return true;
             }
             std::string str = utf8::utf16to8(astNode->tokens.toString());
-            bool isInRange;
-            if constexpr (std::is_same<T, int32_t>()) {
+            bool isNotInRange;
+            if constexpr (std::is_same<T, float>()) {
                 char *end;
-                std::intmax_t value = std::strtoimax(str.c_str(), &end, 10);
-                isInRange = end == str.c_str() || *end != '\0' ||
-                            value < min.value_or(std::numeric_limits<int32_t>::lowest()) ||
-                            value > max.value_or(std::numeric_limits<int32_t>::max());
+                T value = std::strtof(str.c_str(), &end);
+                isNotInRange = end == str.c_str() || *end != '\0' ||
+                               value == HUGE_VALF || value == -HUGE_VALF ||
+                               value < min.value_or(std::numeric_limits<T>::lowest()) ||
+                               value > max.value_or(std::numeric_limits<T>::max());
+            } else if constexpr (std::is_same<T, double>()) {
+                char *end;
+                T value = std::strtod(str.c_str(), &end);
+                isNotInRange = end == str.c_str() || *end != '\0' ||
+                               value == HUGE_VAL || value == -HUGE_VAL ||
+                               value < min.value_or(std::numeric_limits<T>::lowest()) ||
+                               value > max.value_or(std::numeric_limits<T>::max());
+            } else if constexpr (std::is_same<T, long double>()) {
+                char *end;
+                T value = std::strtold(str.c_str(), &end);
+                isNotInRange = end == str.c_str() || *end != '\0' ||
+                               value == HUGE_VALL || value == -HUGE_VALL ||
+                               value < min.value_or(std::numeric_limits<T>::lowest()) ||
+                               value > max.value_or(std::numeric_limits<T>::max());
             } else {
                 char *end;
-                float value = std::strtof(str.c_str(), &end);
-                isInRange = end == str.c_str() || *end != '\0' ||
-                            value == HUGE_VALF || value == -HUGE_VALF ||
-                            value < min.value_or(std::numeric_limits<float>::lowest()) ||
-                            value > max.value_or(std::numeric_limits<float>::max());
+                intmax_t value = std::strtoimax(str.c_str(), &end, 10);
+                isNotInRange = end == str.c_str() || *end != '\0' ||
+                               value < min.value_or(std::numeric_limits<T>::lowest()) ||
+                               value > max.value_or(std::numeric_limits<T>::max());
             }
-            if (!HEDLEY_UNLIKELY(isInRange)) {
+            if (HEDLEY_UNLIKELY(isNotInRange)) {
                 idErrorReasons.push_back(
                         ErrorReason::idError(astNode->tokens,
                                              std::u16string(u"数值不在范围")
