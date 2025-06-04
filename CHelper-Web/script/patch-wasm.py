0402bc300683ce78340b30cc835953e3a11011a9
Yancey <1709185482@qq.com>
2025-06-04 21:15:12 +0800

    improve CHelper-Web speed

--- CHelper-Web/script/patch-wasm.py
+++ CHelper-Web/script/patch-wasm.py
@@ -18,71 +18,102 @@ for buildDir in buildDirs:
     )
     content = content.replace('\n', '')
     content += ('''
+var UTF16ToString = (ptr) => {
+    let str = '';
+    let i = ptr >> 1;
+    while (true) {
+        const codeUnit = HEAPU16[i];
+        if (codeUnit === 0) break;
+        str += String.fromCharCode(codeUnit);
+        ++i;
+    }
+    return str;
+};
+
+var stringToUTF16 = (str) => {
+    const outPtr = _malloc((str.length + 1) * 2);
+    var start = outPtr / 2;
+    var end  = start + str.length;
+    var i = start;
+    while (i < end) {
+        HEAPU16[i] = str.charCodeAt(i - start);
+        ++i;
+    }
+    HEAPU16[i] = 0;
+    return outPtr;
+};
+
 export class CHelperCore {
     constructor(cpack) {
-        const cpackPtr = Module._malloc(cpack.byteLength);
+        const cpackPtr = _malloc(cpack.byteLength);
         HEAP8.set(cpack, cpackPtr);
-        this._corePtr = Module._init(cpackPtr, cpack.byteLength);
-        Module._free(cpackPtr);
+        this._corePtr = _init(cpackPtr, cpack.byteLength);
+        _free(cpackPtr);
         if (this._corePtr === 0) {
             throw "fail to init CHelper core";
         }
     }
 
     release() {
-        Module._release(this._corePtr);
+        _release(this._corePtr);
         this._corePtr = 0;
     }
 
     onTextChanged(content, index) {
-        Module.ccall("onTextChanged", null, ["number", "string", "number"], [this._corePtr, content, index]);
+        const contentPtr = stringToUTF16(content);
+        _onTextChanged(this._corePtr, contentPtr, index);
+        _free(contentPtr);
     }
 
     onSelectionChanged(index) {
-        Module._onSelectionChanged(this._corePtr, index);
+        _onSelectionChanged(this._corePtr, index);
     }
 
     getStructure() {
-        return Module.ccall("getStructure", "string", ["number"], [this._corePtr]);
+        return UTF16ToString(_getStructure(this._corePtr));
     }
 
     getDescription() {
-        return Module.ccall("getDescription", "string", ["number"], [this._corePtr]);
+        return UTF16ToString(_getDescription(this._corePtr));
     }
 
     getErrorReason() {
-        return Module.ccall("getErrorReason", "string", ["number"], [this._corePtr]);
+        return UTF16ToString(_getErrorReason(this._corePtr));
     }
 
     getSuggestionSize() {
-        return Module._getSuggestionSize(this._corePtr);
+        return _getSuggestionSize(this._corePtr);
     }
 
     getSuggestionTitle(which) {
-        return Module.ccall("getSuggestionTitle", "string", ["number", "number"], [this._corePtr, which]);
+        return UTF16ToString(_getSuggestionTitle(this._corePtr, which));
     }
 
     getSuggestionDescription(which) {
-        return Module.ccall("getSuggestionDescription", "string", ["number", "number"], [this._corePtr, which]);
+        return UTF16ToString(_getSuggestionDescription(this._corePtr, which));
     }
 
     onSuggestionClick(which) {
-        return Module.ccall("onSuggestionClick", null, ["number", "number"], [this._corePtr, which]);
+        _onSuggestionClick(this._corePtr, which);
     }
 
     getStringAfterSuggestionClick() {
-        return Module.ccall("getStringAfterSuggestionClick", "string", ["number"], [this._corePtr]);
+        return UTF16ToString(_getStringAfterSuggestionClick(this._corePtr));
     }
 
     getSelectionAfterSuggestionClick() {
-        return Module.ccall("getSelectionAfterSuggestionClick", "number", ["number"], [this._corePtr]);
+        return UTF16ToString(_getSelectionAfterSuggestionClick(this._corePtr));
     }
 }'''
                 .replace(' ', '')
                 .replace('\n', '')
+                .replace('const', 'const ')
+                .replace('let', 'var ')
+                .replace('var', 'var ')
+                .replace('return', 'return ')
                 .replace('exportclassCHelperCore', 'export class CHelperCore')
-                .replace('constcpackPtr', 'const cpackPtr')
                 .replace('failtoinitCHelpercore', 'fail to init CHelper core')
-                .replace('returnModule', 'return Module'))
+                .replace('const ructor', 'constructor')
+                )
     with open(path.join(buildDir, 'libCHelperWeb.js'), 'w') as fp:
         fp.write(content)
