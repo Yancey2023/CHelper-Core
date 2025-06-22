76d8de9eea8ca73d13be069ad35d0d869fa32720
Yancey <1709185482@qq.com>
2025-06-23 03:27:04 +0800

    fix compile error

--- CHelper-Core/src/chelper/CHelperCore.cpp
+++ CHelper-Core/src/chelper/CHelperCore.cpp
@@ -18,11 +18,11 @@ namespace CHelper {
 
     CHelperCore *CHelperCore::create(const std::function<std::unique_ptr<CPack>()> &getCPack) {
         try {
-#ifndef SPDLOG_INFO
+#if SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
             const auto start = std::chrono::high_resolution_clock::now();
 #endif
             std::unique_ptr<CPack> cPack = getCPack();
-#ifndef SPDLOG_INFO
+#if SPDLOG_ACTIVE_LEVEL != SPDLOG_LEVEL_OFF
             const auto end = std::chrono::high_resolution_clock::now();
 #endif
             SPDLOG_INFO("CPack load successfully ({})", fmt::styled(std::chrono::duration_cast<std::chrono::milliseconds>(end - start), fg(fmt::color::medium_purple)));
