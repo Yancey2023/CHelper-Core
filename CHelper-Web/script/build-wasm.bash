cb8754125e082612a72404a3364b145d3d50c7a7
Yancey <1709185482@qq.com>
2025-08-18 01:47:56 +0800

    use Ninja to build wasm

--- CHelper-Web/script/build-wasm.bash
+++ CHelper-Web/script/build-wasm.bash
@@ -6,16 +6,16 @@ git pull
 ./emsdk install latest
 ./emsdk activate latest
 source ./emsdk_env.sh
-sudo apt-get install -y cmake
+sudo apt-get install -y cmake ninja-build
 cd ..
 
-cmake -B cmake-build-emscripten-release -DCMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE="./emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -G "Unix Makefiles"
+cmake -B cmake-build-emscripten-release -DCMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE="./emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -G "Ninja"
 cd cmake-build-emscripten-release
 cmake --build . --target CHelperWeb --parallel
 emcc libCHelperWeb.a include/CHelper-Core/libCHelperNoFilesystemCore.a include/CHelper-Core/include/fmt/libfmt.a include/CHelper-Core/include/spdlog/libspdlog.a include/CHelper-Core/include/xxHash/cmake_unofficial/libxxhash.a -O3 -o libCHelperWeb.js -s FILESYSTEM=0 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH -s ENVIRONMENT="web" -s EXPORTED_FUNCTIONS="['_init','_release','_onTextChanged','_onSelectionChanged','_getDescription','_getErrorReasons','_getSuggestionSize','_getSuggestion','_getAllSuggestions','_onSuggestionClick','_getSyntaxTokens','_malloc','_free']" -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=[]"
 cd ..
 
-cmake -B cmake-build-emscripten-minsizerel -DCMAKE_BUILD_TYPE=MinSizeRel -D CMAKE_TOOLCHAIN_FILE="./emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -G "Unix Makefiles"
+cmake -B cmake-build-emscripten-minsizerel -DCMAKE_BUILD_TYPE=MinSizeRel -D CMAKE_TOOLCHAIN_FILE="./emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -G "Ninja"
 cd cmake-build-emscripten-minsizerel
 cmake --build . --target CHelperWeb --parallel
 emcc libCHelperWeb.a include/CHelper-Core/libCHelperNoFilesystemCore.a include/CHelper-Core/include/fmt/libfmt.a include/CHelper-Core/include/spdlog/libspdlog.a include/CHelper-Core/include/xxHash/cmake_unofficial/libxxhash.a -Os -o libCHelperWeb.js -s FILESYSTEM=0 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH -s ENVIRONMENT="web" -s EXPORTED_FUNCTIONS="['_init','_release','_onTextChanged','_onSelectionChanged','_getDescription','_getErrorReasons','_getSuggestionSize','_getSuggestion','_getAllSuggestions','_onSuggestionClick','_getSyntaxTokens','_malloc','_free']" -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=[]"
