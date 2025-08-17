3c6aecafb114baa34998e76b936f6f840342f330
Yancey <1709185482@qq.com>
2025-08-18 00:24:15 +0800

    update wasm build script

--- CHelper-Web/script/build-wasm.bat
+++ CHelper-Web/script/build-wasm.bat
@@ -5,20 +5,20 @@ call cd ..
 
 @REM 编译Release模式
 echo ----- [Release] 加载CMake项目 -----
-call emcmake cmake -B cmake-build-emscripten-release -DCMAKE_BUILD_TYPE=Release
+call cmake -B cmake-build-emscripten-release -DCMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE="D:\emsdk\upstream\emscripten\cmake\Modules\Platform\Emscripten.cmake" -G "MinGW Makefiles"
 call cd cmake-build-emscripten-release
 echo ----- [Release] 编译CMake项目 -----
-call emmake make CHelperWeb -j
+call cmake --build . --target CHelperWeb --parallel
 echo ----- [Release] 链接wasm -----
 call emcc libCHelperWeb.a include/CHelper-Core/libCHelperNoFilesystemCore.a include/CHelper-Core/include/fmt/libfmt.a include/CHelper-Core/include/spdlog/libspdlog.a include/CHelper-Core/include/xxHash/cmake_unofficial/libxxhash.a -O3 -o libCHelperWeb.js -s FILESYSTEM=0 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH -s ENVIRONMENT="web" -s EXPORTED_FUNCTIONS="['_init','_release','_onTextChanged','_onSelectionChanged','_getDescription','_getErrorReasons','_getSuggestionSize','_getSuggestion','_getAllSuggestions','_onSuggestionClick','_getSyntaxTokens','_malloc','_free']" -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=[]"
 call cd ..
 
 @REM 编译MinSizeRel模式
 echo ----- [MinSizeRel] 加载CMake项目 -----
-call emcmake cmake -B cmake-build-emscripten-minsizerel -DCMAKE_BUILD_TYPE=MinSizeRel
+call cmake -B cmake-build-emscripten-minsizerel -DCMAKE_BUILD_TYPE=MinSizeRel -D CMAKE_TOOLCHAIN_FILE="D:\emsdk\upstream\emscripten\cmake\Modules\Platform\Emscripten.cmake" -G "MinGW Makefiles"
 call cd cmake-build-emscripten-minsizerel
 echo ----- [MinSizeRel] 编译CMake项目 -----
-call emmake make CHelperWeb -j
+call cmake --build . --target CHelperWeb --parallel
 echo ----- [MinSizeRel] 链接wasm -----
 call emcc libCHelperWeb.a include/CHelper-Core/libCHelperNoFilesystemCore.a include/CHelper-Core/include/fmt/libfmt.a include/CHelper-Core/include/spdlog/libspdlog.a include/CHelper-Core/include/xxHash/cmake_unofficial/libxxhash.a -Os -o libCHelperWeb.js -s FILESYSTEM=0 -s DISABLE_EXCEPTION_CATCHING=1 -s ALLOW_MEMORY_GROWTH -s ENVIRONMENT="web" -s EXPORTED_FUNCTIONS="['_init','_release','_onTextChanged','_onSelectionChanged','_getDescription','_getErrorReasons','_getSuggestionSize','_getSuggestion','_getAllSuggestions','_onSuggestionClick','_getSyntaxTokens','_malloc','_free']" -s WASM=1 -s "EXPORTED_RUNTIME_METHODS=[]"
 call cd ..
